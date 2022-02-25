3ad669f8632934bb02990aff8950476ead9d6d61
devopsguru20 <72325886+devopsguru20@users.noreply.github.com>
2022-02-25 10:36:45 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,13 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "kittu0010"
-  acl    = "private"
+  bucket = "my-tf-test-bucket001"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
 }
